63a5225dd0d4d3bbf58fb92462263d85a3bec3e4
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-03-05 18:29:49 -0500

    added basic schema

---
+++ assets/schema.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products
+(
+    item_id INT NOT NULL,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR (50) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES
+    ("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Iphone Case", "Cell Phone & Accessorires", 7.99, 100)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("The Lion King DVD", "Video - Movie", 19.99, 500)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Lap Desk", "Office & School Supplies", 25.00, 50)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
+
+INSERT INTO products
+    (product_name, department_name, price,stock_quantity)
+VALUES("Echo Dot", "Electronic", 49.99, 1000)
