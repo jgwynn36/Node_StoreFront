880d0ad9d58beb9b6b261b434264ad99d9ab93d0
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-03-09 17:22:21 -0500

    fix syntex error in sql code

--- assets/schema.sql
+++ assets/schema.sql
@@ -11,45 +11,45 @@ CREATE TABLE products
     department_name VARCHAR (50) NULL,
     price DECIMAL(10,2) NULL,
     stock_quantity INT NULL,
-    PRIMARY KEY (item_id)
+    PRIMARY KEY(item_id)
 );
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
 VALUES
-    ("Echo Dot", "Electronic", 49.99, 1000)
+    ("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Iphone Case", "Cell Phone & Accessorires", 7.99, 100)
+VALUES("Iphone Case", "Cell Phone & Accessorires", 7.99, 100);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("The Lion King DVD", "Video - Movie", 19.99, 500)
+VALUES("The Lion King DVD", "Video - Movie", 19.99, 500);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Lap Desk", "Office & School Supplies", 25.00, 50)
+VALUES("Lap Desk", "Office & School Supplies", 25.00, 50);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Echo Dot", "Electronic", 49.99, 1000)
+VALUES("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Echo Dot", "Electronic", 49.99, 1000)
+VALUES("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Echo Dot", "Electronic", 49.99, 1000)
+VALUES("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Echo Dot", "Electronic", 49.99, 1000)
+VALUES("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
-VALUES("Echo Dot", "Electronic", 49.99, 1000)
+VALUES("Echo Dot", "Electronic", 49.99, 1000);
 
 INSERT INTO products
     (product_name, department_name, price,stock_quantity)
